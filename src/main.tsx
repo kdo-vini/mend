abf6c1721918e14166ae11fa1bb478e5e1859d42
Vinicius <kdo.vini@gmail.com>
2026-08-04 02:56:54 -0300

    fix: align app entry with runtime dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthGate } from "./components/AuthGate";
 import "./styles.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 30_000, retry: 1 } },
-});
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthGate>
-          <App />
-        </AuthGate>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AuthGate>
+        <App />
+      </AuthGate>
+    </BrowserRouter>
   </StrictMode>,
 );
